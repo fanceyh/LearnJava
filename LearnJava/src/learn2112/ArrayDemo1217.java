c5f9e4aebd4973e96f0c774a0bec4dd82b56e9dc
hcf <fanceyhan@outlook.com>
2021-12-17 15:49:25 +0800

    FirstCommit

---
+++ LearnJava/src/learn2112/ArrayDemo1217.java
@@ -0,0 +1,14 @@
+package learn2112;
+
+public class ArrayDemo1217 {
+    public static void main(String[] args) {
+        int [] arr = new int[3];
+        System.out.println(arr);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[0]);
+        System.out.println("~~~~~~");
+        int[] brr = {1,2,3};
+        System.out.println(brr[2]);
+    }
+}
---
+++ jdk17Moudle/src/firstPack/HW.java
@@ -0,0 +1,7 @@
+package firstPack;
+
+public class HW {
+    public static void main(String[] args) {
+        System.out.println("HW$$$$$$aa");
+    }
+}
