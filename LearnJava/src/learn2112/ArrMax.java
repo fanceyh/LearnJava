a5c5a77c4f7434bac8bc6ed7a0b28b808b11ad73
hcf <fanceyhan@outlook.com>
2021-12-17 16:23:07 +0800

    added ArrayMax class.

---
+++ LearnJava/src/learn2112/ArrMax.java
@@ -0,0 +1,17 @@
+package learn2112;
+
+public class ArrMax {
+    public static void main(String[] args) {
+        int[] arr = {1,9,51,202,7};
+        arrMax(arr);
+        System.out.println(arrMax(arr)+"aaa");
+    }
+    public static int arrMax(int[] arr) {
+        int max= arr[0];
+        for(int x=1;x<arr.length;x++) {
+            if (max < arr[x])
+            max= arr[x];
+        }
+        return max;
+    }
+}
