335144802d3f9b4a60f9f711df723ba1b6b1d38f
hcf <fanceyhan@outlook.com>
2021-12-20 15:18:32 +0800

    打平均分v1.0.

---
+++ LearnJava/src/learn2112class/phone.java
@@ -0,0 +1,30 @@
+package learn2112class;
+
+public class phone {
+    private String brand;
+    private int size;
+    public phone(){
+        brand="brand";
+        size=-1;
+    }
+    public phone(String brand,int size){
+        this.brand=brand;
+        this.size=size;
+    }
+    public String getBrand(){
+        return brand;
+    }
+    public void setBrand(String brand){
+        this.brand=brand;
+    }
+    public int getSize(){
+        return size;
+    }
+    public void setSize(int size){
+        this.size=size;
+    }
+    public void show(){
+        System.out.println("手机品牌："+brand);
+        System.out.println("手机size:"+size);
+    }
+}
---
+++ LearnJava/src/learn2112class/phoneDemo.java
@@ -0,0 +1,14 @@
+package learn2112class;
+
+public class phoneDemo {
+    public static void main(String[] args) {
+        phone a=new phone();
+        a.show();
+        a.setBrand("Xiaomi");
+        a.setSize(5);
+        a.show();
+        phone b=new phone("huawei",7);
+        b.show();
+
+    }
+}
