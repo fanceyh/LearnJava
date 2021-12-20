232cede5ee480f5c1c690a0cf9de647b868119c0
hcf <fanceyhan@outlook.com>
2021-12-20 11:44:11 +0800

    打平均分v1.0.

--- LearnJava/src/learn2112/Score.java
+++ LearnJava/src/learn2112/Score.java
@@ -14,8 +14,19 @@ public class Score {
         }
         int a=max(score);
         System.out.println(a);
+        System.out.println("length"+score.length);
+        System.out.println("avg is:"+avg(score)+'.');
 
     }
+    //-----------------
+    public static int sum(int[]arr){
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
+    //-----------------
     public static int max (int[]arr){
         int i=1;
         int max= arr[0];
@@ -27,6 +38,20 @@ public class Score {
 
         }
         return max;
+    }
+    //-----------------
+    public static int min(int[]arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+    //-----------------
+    public static int avg(int[]arr){
+        return (sum(arr)-max(arr)-min(arr))/(arr.length-2);
     }
 
 }
