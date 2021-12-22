9092c2331864640fb508905936d3b96fb8d73242
hcf <fanceyhan@outlook.com>
2021-12-22 16:42:27 +0800

    Calender对象方法的使用.

---
+++ LearnJava/src/Calender/FebDays.java
@@ -0,0 +1,19 @@
+package Calender;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class FebDays {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("请输入您要查询的年份：");
+        int year= sc.nextInt();
+        Calendar a=Calendar.getInstance();
+        a.set(year,2,1);
+        a.add(5,-1);//field可以写成Calender.Date.
+        int b=a.get(Calendar.DATE);
+        System.out.println(b);
+
+
+    }
+}
